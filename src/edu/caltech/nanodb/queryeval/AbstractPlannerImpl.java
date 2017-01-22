8894e43769665760eea83d1a351bae7aa4359a8f
dqu123 <davidqu12345@gmail.com>
2017-01-22 00:28:46 -0800

    Move makeSimpleSelect() from SimplePlanner.java to AbstractPlannerImpl.java

--- src/edu/caltech/nanodb/queryeval/AbstractPlannerImpl.java
+++ src/edu/caltech/nanodb/queryeval/AbstractPlannerImpl.java
@@ -1,9 +1,11 @@
 package edu.caltech.nanodb.queryeval;
 
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import edu.caltech.nanodb.plannodes.*;
 import org.apache.log4j.Logger;
 
 import edu.caltech.nanodb.queryast.FromClause;
@@ -11,6 +13,9 @@ import edu.caltech.nanodb.queryast.SelectClause;
 import edu.caltech.nanodb.queryast.SelectValue;
 import edu.caltech.nanodb.expressions.Expression;
 import edu.caltech.nanodb.expressions.FunctionCall;
+
+import edu.caltech.nanodb.relations.JoinType;
+import edu.caltech.nanodb.relations.TableInfo;
 import edu.caltech.nanodb.storage.StorageManager;
 
 
@@ -34,4 +39,50 @@ public abstract class AbstractPlannerImpl implements Planner {
     public void setStorageManager(StorageManager storageManager) {
         this.storageManager = storageManager;
     }
+
+    /**
+     * Constructs a simple select plan that reads directly from a table, with
+     * an optional predicate for selecting rows.
+     * <p>
+     * While this method can be used for building up larger <tt>SELECT</tt>
+     * queries, the returned plan is also suitable for use in <tt>UPDATE</tt>
+     * and <tt>DELETE</tt> command evaluation.  In these cases, the plan must
+     * only generate tuples of type {@link edu.caltech.nanodb.storage.PageTuple},
+     * so that the command can modify or delete the actual tuple in the file's
+     * page data.
+     *
+     * @param tableName The name of the table that is being selected from.
+     *
+     * @param predicate An optional selection predicate, or {@code null} if
+     *        no filtering is desired.
+     *
+     * @return A new plan-node for evaluating the select operation.
+     *
+     * @throws IOException if an error occurs when loading necessary table
+     *         information.
+     */
+    public SelectNode makeSimpleSelect(String tableName, Expression predicate,
+        List<SelectClause> enclosingSelects) throws IOException {
+        if (tableName == null)
+            throw new IllegalArgumentException("tableName cannot be null");
+
+        if (enclosingSelects != null) {
+            // If there are enclosing selects, this subquery's predicate may
+            // reference an outer query's value, but we don't detect that here.
+            // Therefore we will probably fail with an unrecognized column
+            // reference.
+            logger.warn("Currently we are not clever enough to detect " +
+                "correlated subqueries, so expect things are about to break...");
+        }
+
+        // Open the table.
+        TableInfo tableInfo = storageManager.getTableManager().openTable(tableName);
+
+        // Make a SelectNode to read rows from the table, with the specified
+        // predicate.
+        SelectNode selectNode = new FileScanNode(tableInfo, predicate);
+        selectNode.prepare();
+        return selectNode;
+    }
+
 }
--- src/edu/caltech/nanodb/queryeval/SimplePlanner.java
+++ src/edu/caltech/nanodb/queryeval/SimplePlanner.java
@@ -115,50 +115,5 @@ public class SimplePlanner extends AbstractPlannerImpl {
         return result;
     }
 
-
-    /**
-     * Constructs a simple select plan that reads directly from a table, with
-     * an optional predicate for selecting rows.
-     * <p>
-     * While this method can be used for building up larger <tt>SELECT</tt>
-     * queries, the returned plan is also suitable for use in <tt>UPDATE</tt>
-     * and <tt>DELETE</tt> command evaluation.  In these cases, the plan must
-     * only generate tuples of type {@link edu.caltech.nanodb.storage.PageTuple},
-     * so that the command can modify or delete the actual tuple in the file's
-     * page data.
-     *
-     * @param tableName The name of the table that is being selected from.
-     *
-     * @param predicate An optional selection predicate, or {@code null} if
-     *        no filtering is desired.
-     *
-     * @return A new plan-node for evaluating the select operation.
-     *
-     * @throws IOException if an error occurs when loading necessary table
-     *         information.
-     */
-    public SelectNode makeSimpleSelect(String tableName, Expression predicate,
-        List<SelectClause> enclosingSelects) throws IOException {
-        if (tableName == null)
-            throw new IllegalArgumentException("tableName cannot be null");
-
-        if (enclosingSelects != null) {
-            // If there are enclosing selects, this subquery's predicate may
-            // reference an outer query's value, but we don't detect that here.
-            // Therefore we will probably fail with an unrecognized column
-            // reference.
-            logger.warn("Currently we are not clever enough to detect " +
-                "correlated subqueries, so expect things are about to break...");
-        }
-
-        // Open the table.
-        TableInfo tableInfo = storageManager.getTableManager().openTable(tableName);
-
-        // Make a SelectNode to read rows from the table, with the specified
-        // predicate.
-        SelectNode selectNode = new FileScanNode(tableInfo, predicate);
-        selectNode.prepare();
-        return selectNode;
-    }
 }
 
