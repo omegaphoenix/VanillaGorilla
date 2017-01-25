171d1efada35352325521b7e375550ee2a995b21
Matthew Jin <massieujin@gmail.com>
2017-01-24 23:50:25 -0800

    Finish AggregateProcessor

--- src/edu/caltech/nanodb/expressions/AggregateProcessor.java
+++ src/edu/caltech/nanodb/expressions/AggregateProcessor.java
@@ -1,27 +1,36 @@
 package edu.caltech.nanodb.expressions;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import edu.caltech.nanodb.functions.ScalarFunction;
 import edu.caltech.nanodb.functions.AggregateFunction;
-
+import edu.caltech.nanodb.expressions.ColumnValue;
 
 public class AggregateProcessor implements ExpressionProcessor {
 
-    // TODO: map
+    Map<String, FunctionCall> aggregates;
+
+    public AggregateProcessor() {
+        aggregates = new HashMap<>();
+    }
 
     public void enter(Expression e) {
+        return;
+    }
+
+    public Expression leave(Expression e) {
         if (e instanceof FunctionCall) {
             FunctionCall call = (FunctionCall) e;
             ScalarFunction f = call.getFunction();
             if (f instanceof AggregateFunction) {
-                // Rename?
-
+                int count = aggregates.size();
+                String uuid = String.format("#A%d", count);
+                ColumnValue renamed = new ColumnValue(new ColumnName(uuid));
+                aggregates.put(uuid, call);
+                return renamed;
             }
         }
-    }
-
-    public Expression leave(Expression e) {
-        // TODO: return renamed expression?
         return e;
     }
-
 }
