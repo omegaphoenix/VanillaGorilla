686d5cc81499636ca8a3e039383f6ba32d74ede1
dqu123 <davidqu12345@gmail.com>
2017-02-08 22:46:38 -0800

    Add different permutations for hw4/test.sql

--- schemas/hw4/test.sql
+++ schemas/hw4/test.sql
@@ -4,6 +4,18 @@ EXPLAIN SELECT state_id FROM states;
 EXPLAIN SELECT store_id, property_costs
         FROM stores, cities, states
         WHERE stores.city_id = cities.city_id AND 
-              cities.state_id = states.state_id aND
+              cities.state_id = states.state_id AND
               state_name = 'Oregon' AND
               property_costs > 500000;
+EXPLAIN SELECT store_id, property_costs
+        FROM cities, states, stores
+        WHERE stores.city_id = cities.city_id AND 
+              state_name = 'Oregon' AND
+              cities.state_id = states.state_id AND
+              property_costs > 500000;
+EXPLAIN SELECT store_id, property_costs
+        FROM  states, stores, cities
+        WHERE stores.city_id = cities.city_id AND 
+              cities.state_id = states.state_id AND
+              property_costs > 500000 AND
+              state_name = 'Oregon'; 
