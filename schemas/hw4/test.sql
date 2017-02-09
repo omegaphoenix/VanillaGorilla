3cec9fc80f2cce65048c81723113be86ad14e510
dqu123 <davidqu12345@gmail.com>
2017-02-08 22:25:06 -0800

    Add hw4/test.sql for testing

---
+++ schemas/hw4/test.sql
@@ -0,0 +1,9 @@
+ANALYZE states;
+EXPLAIN SELECT * FROM states;
+EXPLAIN SELECT state_id FROM states;
+EXPLAIN SELECT store_id, property_costs
+        FROM stores, cities, states
+        WHERE stores.city_id = cities.city_id AND 
+              cities.state_id = states.state_id aND
+              state_name = 'Oregon' AND
+              property_costs > 500000;
